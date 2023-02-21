8591903066aaa70e20d7194be1583ad45d82dfe9
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-02-21 23:28:30 +0100

    get all posts

--- controllers/postController.js
+++ controllers/postController.js
@@ -10,5 +10,15 @@ exports.postCtrl = {
         } catch (error) {
             return res.status(500).json({ message: error.message })
         }
+    },
+    getAllPosts: async (req, res) => {
+        try {
+            const posts = await Post.find()
+                .populate("user", "first_name last_name email picture username gender")
+                .sort({ createdAt: -1 })
+            res.status(200).json(posts)
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+        }
     }
 }
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,6 +3,7 @@ const { postCtrl } = require("../controllers/postController")
 const { protect } = require("../middleware/protect")
 
 router.post("/create", protect, postCtrl.createPost)
+router.get("/getAllPosts", protect, postCtrl.getAllPosts)
 
 
 module.exports = router
