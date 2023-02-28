7d5d240f6870c67c637364c3009ae26861c95b91
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-02-28 19:43:17 +0100

    after install script updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,6 @@ cd /home/ec2-user/ineedsomething-v3-backend
 sudo rm -rf .env
 sudo rm -rf .env.production
 aws s3 sync s3://needsomething-env-files/production .
+sudo cp .env.production .env
 sudo pm2 delete all
 npm install
