2fd6a673d81d6f45df47cc189f6d719fa8596a88
ruyisbaros <ahmet.erdonmez77@gmail.com>
2023-03-01 15:13:21 +0100

    server set

--- server.js
+++ server.js
@@ -29,8 +29,8 @@ app.use(
             //domain: "https://ineedsomething.org",
             keys: [process.env.KEY_ONE, process.env.KEY_TWO],
             maxAge: 7 * 24 * 60 * 60 * 1000, //7 days
-            //secure: true,
-            sameSite: "lax",//use for production
+            secure: true,
+            sameSite: "none",//use for production
         })
 );
 app.use(bodyParser.json());
